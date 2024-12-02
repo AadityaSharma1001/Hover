e5d91b9617e7f7ec00256f01cd67460ba5dff9ef
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-02 12:21:49 +0530

    configured setup and connected user register routes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { connectDB } from './db/db.js';
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config();
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+connectDB();
+
 const port = process.env.PORT || 3000;
+
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');

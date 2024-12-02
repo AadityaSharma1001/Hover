478b22ddbebb5d862993febcf90a247e141bd33d
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-02 13:08:16 +0530

    added login controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -25,3 +25,31 @@ export const register = async (req, res) => {
         res.status(400).send(error);
     }
 }
+
+export const login = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).send({error: errors.array()});
+        }
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select('+password');
+        
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const token = user.generateAuthToken();
+        res.status(200).json({user, token});
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}

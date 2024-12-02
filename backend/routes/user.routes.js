cf596b3c4bacc23b9b2a4c2c49890edf1c168252
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-02 15:16:02 +0530

    added new routes for getting profile, login  and logout

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { register, login } from '../controllers/user.controller.js';
+import { register, login, getProfile, logout } from '../controllers/user.controller.js';
+import authUser  from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 
@@ -14,5 +15,9 @@ router.post('/login', [
   body('email').isEmail().withMessage('Email is not valid'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ], login);
+
+router.get('/profile', authUser, getProfile);
+
+router.get('/logout', authUser, logout);
 
 export default router;

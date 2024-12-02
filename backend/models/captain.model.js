a214dd1f697d279d0785706f85829202d8e525a7
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-02 16:42:18 +0530

    made model for captain

---
+++ backend/models/captain.model.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const captain = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlenght: [3, 'First name must be at least 3 characters long']
+        },
+        lastname: {
+            type: String,
+            required: true,
+            
+        }
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid']
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlenght: [6, 'Password must be at least 6 characters long']
+    },
+
+    socketId: {
+        type: String,
+    },
+
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'inactive'
+    },
+
+    vechicle: {
+        color: {
+            type: String,
+            required: true,
+            minlenght: [3, 'Color must be at least 3 characters long']
+        },
+        plate: {
+            type: String,
+            required: true,
+            minlenght: [3, 'Plate must be at least 3 characters long']
+        },
+        platePhoto: {
+            type: String,
+            required: true
+        },
+        ownerPhoto: {
+            type: String,
+            required: true
+        },
+        capacity: {
+            type: Number,
+            required: true,
+            min: [1, 'Capacity must be at least 1']
+        },
+        type: {
+            type: String,
+            required: true,
+            enum: ['car', 'motorcycle','auto rickshaw']
+        }
+    },
+    location: {
+        lat: {
+            type: Number,
+            required: true
+        },
+        lng: {
+            type: Number,
+            required: true
+        }
+    }
+});
+
+captain.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    return token;
+}
+
+captain.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+captain.statics.hashPassword = async function (password) {
+    return bcrypt.hash(password, 8);
+}
+
+const Captain = mongoose.model('Captain', captain);
+
+export default Captain;

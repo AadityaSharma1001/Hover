b72fff87967add0f1eb2db33d6e63cdc3669aca0
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-05 22:35:25 +0530

    added Security Layer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from "./pages/Home"
+import Start from "./pages/Start"
 import UserSignup from "./pages/UserSignup"
 import UserLogin from "./pages/UserLogin"
 import CaptainSignup from "./pages/CaptainSignup"
 import CaptainLogin from "./pages/CaptainLogin"
+import Home from "./pages/Home"
 import { UserDataContext } from './context/UserContext'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
 
 function App() {
 
@@ -13,11 +16,13 @@ function App() {
   return (
     <div >
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Start/>} />
+      <Route path="/home" element={<UserProtectWrapper><Home/></UserProtectWrapper>} />
       <Route path="/signup" element={<UserSignup />} />
       <Route path="/login" element={<UserLogin/>} />
       <Route path="/captain-signup" element={<CaptainSignup />} />
       <Route path="/captain-login" element={<CaptainLogin/>} />
+      <Route path="/user/logout" element={<UserProtectWrapper><UserLogout/></UserProtectWrapper>} />
     </Routes>
     </div>
   )

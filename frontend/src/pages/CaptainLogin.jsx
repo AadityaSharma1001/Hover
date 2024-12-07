5a1b896c8a378d48809bbffb57ef625bcaf3a80c
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-07 22:55:28 +0530

    connected login endpoint for captain

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,15 +1,37 @@
-import React , { useState } from 'react'
+import React , { useState, useContext } from 'react'
 import hover_black from './hover-black.png'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { CaptainDataContext } from '../context/CaptainContext'
 
 const CaptainLogin = () => {
   const [email,setEmail] =useState("");
   const [password,setPassword] =useState("");
-  const [captainData,setCaptainData] =useState({});
+  // const [captainData,setCaptainData] =useState({});
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+
+  const { captain, setCaptain } = useContext(CaptainDataContext);
+
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setCaptainData({email:email,password:password});
+    const captainData = {
+      email,
+      password,
+    };
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/login`,
+      captainData
+    );
+
+    if(response.status === 200){
+      const data = response.data;
+      setCaptain(data);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
+    }
     setEmail("");
     setPassword(""); 
   };

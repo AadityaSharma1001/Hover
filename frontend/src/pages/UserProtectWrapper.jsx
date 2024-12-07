ddfa6719d9aaba48c6e56990d72f80184a72b2a6
AadityaSharma1001 <b23me1001@iitj.ac.in>
2024-12-07 22:58:17 +0530

    added extra layer of protection

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -1,15 +1,41 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+import axios from "axios";
 
 const UserProtectWrapper = ({ children }) => {
 
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
-    if (!token) {
-        useEffect(() => {
-            navigate("/login");
-        }, []);
-    }
+    const { user, setUser } = useContext(UserDataContext);
+    const [ isLoading, setIsLoading ] = useState(true);
+
+    useEffect(() => {
+        if (!token) {
+          navigate("/login");
+        }
+      }, [token]);
+
+      axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => {
+        if(response.status === 200) {
+          setUser(response.data);
+          setIsLoading(false);
+        }
+      }
+      ).catch((error) => {
+        console.error(error);
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }   
     return <>{children}</>;
 };
 

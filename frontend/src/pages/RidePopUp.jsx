96625faacc067cf047f06595067bd3cabecd38c4
AadityaSharma1001 <b23me1001@iitj.ac.in>
2025-05-18 23:52:22 +0530

    A new ride request popup in Captain home page

---
+++ frontend/src/pages/RidePopUp.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const RidePopUp = () => {
+  return (
+    <div>
+      <h5
+        className="p-1 text-center absolute w-[93%] top-0"
+        onClick={() => {
+          props.setConfirmRidePanel(false);
+        }}
+      >
+        <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
+      </h5>
+      <h3 className="font-semibold text-2xl mb-5">A New Ride Available</h3>
+      <div className="flex gap-2 flex-col justify-between items-center">
+        <img
+          className="h-20"
+          src="https://tse4.mm.bing.net/th?id=OIP.ymjpxr4RPlwbLenCbbpYywHaE7&pid=Api&P=0&h=180"
+          alt="car"
+        ></img>
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <i className="text-lg ri-map-pin-2-fill"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm text-gray-600 ">
+                Kankariya Talab, Ahmedabad
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <i className="text-lg ri-map-pin-user-fill"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm text-gray-600 ">
+                Kankariya Talab, Ahmedabad
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <i className="text-lg ri-currency-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">₹193.20</h3>
+              <p className="text-sm text-gray-600 ">
+                Cash Cash
+              </p>
+            </div>
+          </div>
+        </div>
+        <button onClick={() => {
+            
+        }} className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg">Confirm</button>
+        <button onClick={() => {
+            
+        }} className="w-full mt-1 bg-gray-400 text-gray-700 font-semibold p-2 rounded-lg">Ignore</button>
+      </div>
+    </div>
+  )
+}
+
+export default RidePopUp
